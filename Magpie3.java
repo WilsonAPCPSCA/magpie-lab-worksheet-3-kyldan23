e1f739203f666064e7af14c067b4f50e540bbc57
Kyle Dang <30358343+kyldan23@users.noreply.github.com>
2018-01-26 22:13:49 -0800

    Add files via upload

--- Magpie3.java
+++ Magpie3.java
@@ -1,3 +1,4 @@
+package Magpie_Lab3;
 /**
  * A program to carry on conversations with a human user.
  * This version: 
@@ -45,6 +46,21 @@ public class Magpie3
 		{
 			response = "Tell me more about your family.";
 		}
+		else if(findKeyword(statement, "dog",0)>=0) {
+			response="Tell me more about your pets.";
+		}
+		else if(findKeyword(statement, "Mr",0)>=0|| findKeyword(statement, "Ms",0)>=0) {
+			response="What a cool teacher!";
+		}
+		else if(findKeyword(statement, "rock",0)>=0) {
+			System.out.println("I chose paper....HA! Get covered!");
+		}
+		else if(findKeyword(statement, "paper",0)>=0) {
+			System.out.println("I chose scissors....HA! Get cut!");
+		}
+		else if(findKeyword(statement, "scissors",0)>=0) {
+			System.out.println("I chose rock....HA! Get smashed!");
+		}
 		else
 		{
 			response = getRandomResponse();
@@ -71,25 +87,30 @@ public class Magpie3
 	private int findKeyword(String statement, String goal,
 			int startPos)
 	{
-		String phrase = statement.trim().toLowerCase();
-		goal = goal.toLowerCase();
+		String phrase = statement.trim().toLowerCase(); //makes the statement input into lowercase
+		goal = goal.toLowerCase(); //turns the string that it's trying to find to lowercase
 
 		// The only change to incorporate the startPos is in
 		// the line below
-		int psn = phrase.indexOf(goal, startPos);
-
+		int psn = phrase.indexOf(goal, startPos); //starts from index "startPos" to identify the first location
+											      //of goal 
+		
 		// Refinement--make sure the goal isn't part of a
 		// word
-		while (psn >= 0)
+		while (psn >= 0) //psn was the int variable found for the first location of goal
 		{
 			// Find the string of length 1 before and after
 			// the word
 			String before = " ", after = " ";
-			if (psn > 0)
+			if (psn > 0) //does not equal to 0, since 0 will automatically mean that there's 
+						 //nothing that comes before the goal
 			{
-				before = phrase.substring(psn - 1, psn);
+				before = phrase.substring(psn - 1, psn); //String before
+													     //String phrase is used instead of the original statement
 			}
-			if (psn + goal.length() < phrase.length())
+			if (psn + goal.length() < phrase.length()) //checks to make sure that once you add the index of the 
+			//first instance with the length of the goal, that it's less than the length of the whole entire input statement
+		    //if it was greater than the statement, then there would be no characters or anything after the goal is found
 			{
 				after = phrase.substring(
 						psn + goal.length(),
@@ -109,11 +130,13 @@ public class Magpie3
 
 			// The last position didn't work, so let's find
 			// the next, if there is one.
-			psn = phrase.indexOf(goal, psn + 1);
+			psn = phrase.indexOf(goal, psn + 1); 
+			//starting from the next index, jumping over the index you last started from
+			
 
 		}
 
-		return -1;
+		return -1; //returns -1 if the goal is not found
 	}
 
 	/**
